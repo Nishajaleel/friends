42b7ce05facf95319fc43380559a3f22b9654fc0
Nishajaleel <nishanajaleel690@gmail.com>
2022-07-13 14:52:55 +0530

    completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FriendslistComponent } from './friendslist/friendslist.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FriendslistComponent
   ],
   imports: [
     BrowserModule,
